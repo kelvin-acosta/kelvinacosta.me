a0ac11d12627b1f66c4c39ed0546a0d7e7e44e6d
Kelvin Acosta <kacosta@fuzzproductions.com>
2017-01-15 14:26:32 -0500

    Add scroll behavior

--- src/main.js
+++ src/main.js
@@ -10,18 +10,39 @@ import Home from './pages/Home'
 
 Vue.use(VueRouter)
 
+const scrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) {
+    // savedPosition is only available for popstate navigations.
+    return savedPosition
+  } else {
+    const position = {}
+    // new navigation.
+    // scroll to anchor by returning the selector
+    if (to.hash) {
+      position.selector = to.hash
+    }
+    // check if any matched route config has meta that requires scrolling to top
+    if (to.matched.some(m => m.meta.scrollToTop)) {
+      // cords will be used if no selector is provided,
+      // or if the selector didn't match any element.
+      position.x = 0
+      position.y = 0
+    }
+    // if the returned position is falsy or an empty object,
+    // will retain current scroll position.
+    return position
+  }
+}
+
 const routes = [
-  {path: '/', component: Home}
-  // {path: '/Experience', component: Experience},
-  // {path: '/Education', component: Education},
-  // {path: '/Skills', component: Skills},
-  // {path: '/Projects', component: Projects},
-  // {path: '/Contact', component: Contact}
+  {path: '/', component: Home, meta: {scrollToTop: true}}
 ]
 
 const router = new VueRouter({
   history: true,
   hashbang: false,
+  mode: 'history',
+  scrollBehavior,
   routes
 })
 /* eslint-disable no-new */
