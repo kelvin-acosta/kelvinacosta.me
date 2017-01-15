1cf86208b8f9d3650ca26e613746d61999100b18
Kelvin Acosta <kacosta@fuzzproductions.com>
2017-01-15 14:54:52 -0500

    Update font importing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <Navbar></Navbar>
     <Home></Home>
     <Footernav></Footernav>
@@ -22,7 +21,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss'>
+@import url('https://fonts.googleapis.com/css?family=Montserrat');
+
 #app {
   font-family: 'Montserrat', Helvetica, Arial, sans-serif;
   font-style: normal;
