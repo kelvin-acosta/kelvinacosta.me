84ee730f4985eb0e82639b5576e54e4ced0734ad
Kelvin Acosta <kacosta@fuzzproductions.com>
2017-01-15 14:44:52 -0500

    Import sections, add anchor tags

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,21 +1,48 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="main-container">
+    <div id='me'>
+      <me></me>
+    </div>
+    <div id='about'>
+      <about></about>
+    </div>
+    <div id='work'>
+      <experience></experience>
+    </div>
+    <div id='skills'>
+      <skills></skills>
+    </div>
+    <div id='projects'>
+      <projects></projects>
+    </div>
   </div>
 </template>
 
 <script>
+import me from '../sections/Me'
+import about from '../sections/About'
+import experience from '../sections/Experience'
+import skills from '../sections/Skills'
+import projects from '../sections/Projects'
+
 export default {
-  name: 'hello',
+  name: 'Home',
+  components: {
+    me,
+    about,
+    experience,
+    skills,
+    projects
+  },
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+    return {}
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss' scoped>
+.main-container {
 
+}
 </style>
